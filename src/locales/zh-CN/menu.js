70dee5a494d0f2531ff125722c93487bcccb9639
caicai <181424167@qq.com>
2019-06-24 23:27:57 +0800

    no message

--- src/locales/zh-CN/menu.js
+++ src/locales/zh-CN/menu.js
@@ -36,7 +36,6 @@ export default {
   'menu.account': '个人页',
   'menu.account.center': '个人中心',
   'menu.account.settings': '个人设置',
-  'menu.account.trigger': '触发报错',
   'menu.account.logout': '退出登录',
   'menu.editor': '图形编辑器',
   'menu.editor.flow': '流程编辑器',
