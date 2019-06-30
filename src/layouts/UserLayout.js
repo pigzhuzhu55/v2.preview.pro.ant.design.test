8b569e16e33976fb9f1960b95e2b70932f1b00c1
caicai <181424167@qq.com>
2019-06-30 11:59:52 +0800

    no message

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -56,6 +56,22 @@ class UserLayout extends Component {
       <DocumentTitle title={getPageTitle(pathname, breadcrumbNameMap)}>
         <div className={styles.container}>
           <div className={styles.lang}>
+            <span style={{
+              color:'#333'
+            }}>
+              <a title="help" target="_self" href="" style={{
+              color:'#333',
+              padding: '0 8px'
+            }}>申请使用</a>|
+              <a title="privacy" target="_self" href=""style={{
+              color:'#333',
+              padding: '0 8px'
+            }}>移动端</a>|
+              <a title="terms" target="_self" href=""style={{
+              color:'#333',
+              padding: '0 20px 0 8px'
+            }}>官网</a>
+            </span>
               <SelectLang />
           </div>
           <div className={styles.content}>
