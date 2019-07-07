f66f35b119457770f8a660a7f613501492a487a0
caicai <181424167@qq.com>
2019-07-08 00:16:18 +0800

    no message

--- src/pages/Dashboard/Workplace.js
+++ src/pages/Dashboard/Workplace.js
@@ -1,13 +1,11 @@
 import React, { PureComponent } from 'react';
 
-import TablePageHeaderBox1 from './../../components/My/TablePageHeaderBox1';
 import styles from './Workplace.less';
 
 class Workplace extends PureComponent {
   render() {
-    const buttons = [{ text: '新增', type: 'primary' }, { text: '移除' }];
 
-    return <TablePageHeaderBox1 buttons={buttons} />;
+    return <div>首页TODO</div>;
   }
 }
 
